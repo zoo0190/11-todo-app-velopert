32d450859348f7e78542d875e2b4ae265f3f06d9
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2021-02-27 16:36:16 +0900

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => {
+        return (
+          <TodoListItem
+            todo={todo}
+            key={todo.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
